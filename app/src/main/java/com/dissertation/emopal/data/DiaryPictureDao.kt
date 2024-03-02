6ff2d2051ab1f261fe9bf7ba030bebc7a61927a8
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-03-02 13:45:26 +0000

    feat: Diary Database structure implemented (#4)

---
+++ app/src/main/java/com/dissertation/emopal/data/DiaryPictureDao.kt
@@ -0,0 +1,46 @@
+package com.dissertation.emopal.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+/**
+ * The Diary Picture Dao is the interface that contains the methods to interact with the database.
+ * @Author: A.La Fauci
+ * @Date: 02/03/2024
+ */
+@Dao
+interface DiaryPictureDao {
+
+    /**
+     * Inserts a picture in the database.
+     * @param diaryPictureModel The picture to insert.
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPicture(diaryPictureModel: DiaryPictureModel)
+
+    /**
+     * Updates a picture in the database.
+     * @param diaryPictureModel The picture to update.
+     */
+    @Update
+    suspend fun updatePicture(diaryPictureModel: DiaryPictureModel)
+
+    /**
+     * Deletes a picture from the database.
+     * @param diaryPictureModel The picture to delete.
+     */
+    @Delete
+    suspend fun deletePicture(diaryPictureModel: DiaryPictureModel)
+
+    /**
+     * Gets all the pictures from the database.
+     */
+    @Query("SELECT * FROM diary_pictures")
+    fun getAllPictures(): LiveData<List<DiaryPictureModel>>
+
+}
