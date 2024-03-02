2937271f92f58cd6ab486dc6ca748325b655ea01
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-03-02 16:39:26 +0000

    feat: WIP asking user for Camera permissions (#6)

--- app/src/main/java/com/dissertation/emopal/MainActivity.kt
+++ app/src/main/java/com/dissertation/emopal/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.dissertation.emopal
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.dissertation.emopal.ui.theme.EmoPalTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Asking user for permission to use the defined permissions needed
+        // TODO: This should be handled differently with specific components. For later reference.
+        if (!hasPermissions()) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, 0)
+        }
         setContent {
             EmoPalTheme {
-                setContent{
+                setContent {
                     /***
                      * This is the main entry point for the app
                      */
@@ -18,6 +27,29 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    /**
+     * Check if the app has the necessary permissions to run.
+     */
+    private fun hasPermissions(): Boolean {
+        return PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(
+                applicationContext,
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    /**
+     * Companion object to define the permissions needed for the application to run.
+     * In this case the app needs the camera and internet permissions.
+     */
+    companion object {
+        private val PERMISSIONS = arrayOf(
+            android.Manifest.permission.CAMERA,
+            android.Manifest.permission.INTERNET,
+        )
     }
 }
 
