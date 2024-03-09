5ce9035b6df10fbd3ec1d5b88e14d4e465a97a5e
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-03-09 19:21:39 +0000

    feat: TakePicture function implemented and callback setup (#6)

--- app/src/main/java/com/dissertation/emopal/EmoPalApp.kt
+++ app/src/main/java/com/dissertation/emopal/EmoPalApp.kt
@@ -22,6 +22,8 @@ import com.dissertation.emopal.ui.screens.play.PlayScreen
 fun EmoPalApp(
     navController: NavHostController = rememberNavController()
 ) {
+//    val cameraViewModel: CameraViewModel = viewModel()
+
     /**
      * Navigate back to the Home Screen lambda function
      */
@@ -83,9 +85,12 @@ fun EmoPalApp(
                         navController.navigate(Routes.CAMERA.name)
                     })
                 }
-                // TODO: Add Back button function to navigate back to the Diary Screen
                 composable(route = Routes.CAMERA.name) {
-                    CameraView(navigateBackToDiary)
+                    CameraView(
+                        navigateBackToDiary,
+                        // TODO: Experiment without passed ViewModel
+//                        cameraViewModel
+                    )
                 }
             } // End of Nested Navigation Graph for Diary Screen
 
