74244ec3f6365d3b0ed0e2d7ab912e13f6ce0872
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-04-27 14:53:11 +0100

    feat: Added Haptic menu to delete pictures from Diary

--- app/src/main/java/com/dissertation/emopal/ui/components/PictureCase.kt
+++ app/src/main/java/com/dissertation/emopal/ui/components/PictureCase.kt
@@ -5,16 +5,25 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DeleteOutline
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dissertation.emopal.ui.screens.diary.FullScreenImage
@@ -29,6 +38,8 @@ import com.dissertation.emopal.ui.screens.diary.PictureBox
 fun PictureCase(pictures: List<Bitmap>, category: String) {
 
     var activePicture by remember { mutableStateOf<Bitmap?>(null) }
+    var haptics = LocalHapticFeedback.current
+    var contextMenuPhoto by rememberSaveable { mutableStateOf<Bitmap?>(null) }
 
     if (pictures.isEmpty()) {
         Box(
@@ -46,7 +57,14 @@ fun PictureCase(pictures: List<Bitmap>, category: String) {
         Divider()
         LazyRow {
             items(pictures) { picture ->
-                PictureBox(picture, onClick = { activePicture = picture })
+                PictureBox(
+                    picture,
+                    onClick = { activePicture = picture },
+                    onLongClick = {
+                        haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                        contextMenuPhoto = picture
+                    }
+                )
             }
         }
     }
@@ -54,6 +72,29 @@ fun PictureCase(pictures: List<Bitmap>, category: String) {
     if (activePicture != null) {
         FullScreenImage(photo = pictures.first { it == activePicture!! },
             onDismiss = { activePicture = null })
+    }
+
+    if (contextMenuPhoto != null) {
+        PhotoActionsSheet(
+            photo = contextMenuPhoto!!,
+            onDismissSheet = { contextMenuPhoto = null }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun PhotoActionsSheet(
+    @Suppress("UNUSED_PARAMETER") photo: Bitmap,
+    onDismissSheet: () -> Unit
+) {
+    ModalBottomSheet(
+        onDismissRequest = onDismissSheet
+    ) {
+        ListItem(
+            headlineContent = { Text("Delete") },
+            leadingContent = { Icon(Icons.Default.DeleteOutline, null) }
+        )
     }
 }
 
